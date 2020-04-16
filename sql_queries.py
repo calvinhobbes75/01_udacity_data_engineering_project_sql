b80000cf0f4b34f1e0cdd1d6ab51e5e832576cf3
sdlz <sdelopez@free.fr>
2020-04-16 23:08:29 +0200

    Commit initial remote

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,28 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXITS songplays"
+user_table_drop = "DROP TABLE IF EXITS users"
+song_table_drop = "DROP TABLE IF EXITS songs"
+artist_table_drop = "DROP TABLE IF EXITS "
+time_table_drop = "DROP TABLE IF EXITS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+# FACTS table
+songplay_table_create = ("""CREATE TABLE IF NOT EXITS songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent);
 """)
 
-user_table_create = ("""
+# DIMENSIONS tables
+user_table_create = ("""CREATE TABLE IF NOT EXITS users (user_id, first_name, last_name, gender, level);
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXITS songs (song_id, title, artist_id, year, duration);
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE IF NOT EXITS artists (artist_id, name, location, latitude, longitude);
 """)
 
-time_table_create = ("""
+time_table_create = ("""CREATE TABLE IF NOT EXITS time (start_time, hour, day, week, month, year, weekday);
 """)
 
 # INSERT RECORDS
